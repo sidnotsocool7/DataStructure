1e28cc7dcaa9247b4439a749ec1833401975a87a
Siddharth Jain <s.jain@tools.boots>
2019-10-24 07:15:32 +0000

    Unit 1

---
+++ ArithmeticAndMathFunc.js
@@ -0,0 +1,18 @@
+/**
+ * Problem - Arithmetic and Math Functions in javascript
+ *
+ */
+
+function print(value) {
+  console.log(value);
+}
+var x = 3;
+var y = 1.1;
+print(x + y); //4
+print(x * y); //3.3000000000000003
+print((x + y) * (x - y)); //7.789999999999999
+var z = 9;
+print(Math.sqrt(z)); //3
+print(Math.abs(y / x)); //0.3666666666666667
+var k = x * y;
+print(k.toFixed(2)); //3.30
