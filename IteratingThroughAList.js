c3015a36b28312d9577198b1af149394b3af49a3
Siddharth Jain <SiddharthJain>
2019-11-01 10:25:34 +0530

    Unit-3

---
+++ IteratingThroughAList.js
@@ -0,0 +1,15 @@
+/**
+ * Problem - Iterating through a list 
+    Note - 
+    for(names.front(); names.currPos() < names.length(); names.next()){
+        console.log(names.getElement());
+    }
+    We can also traverse a list backward using an iterator
+    for(names.end(); names.currPos() >=0; names.prev()){
+        console.log(names.getElement());
+    }
+    Iterators are used only to move through a list and should not be combined with any functions
+    for adding or removing items from a list. 
+ */
+
+ 
