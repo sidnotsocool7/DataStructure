566304c34054e11cf40633763eca249969acc238
Siddharth Jain <s.jain@tools.boots>
2019-11-01 07:37:53 +0000

    Unit-3

--- AListBasedApp.js
+++ AListBasedApp.js
@@ -17,27 +17,53 @@
  */
 
 //Reading Text Files
-function createArr(file){
-    var arr = read(file).split("\n");
-    for(var i =0; i<arr.length; ++i){
-        arr[i] = arr[i].trim();
-    }
-    return arr;
+function createArr(file) {
+  var arr = read(file).split("\n");
+  for (var i = 0; i < arr.length; ++i) {
+    arr[i] = arr[i].trim();
+  }
+  return arr;
 }
 
 //Using Lists to manage a kiosk
 var movieList = new List();
-for(var i=0; i<movies.length; i++){
-    movieList.append(movies[i]);
+for (var i = 0; i < movies.length; i++) {
+  movieList.append(movies[i]);
 }
 
 //Native types
-function displayList(list){
-    for(list.front(); list.currPos() < list.length(); list.next()){
-        console.log(list.getElement());
-    }
+function displayList1(list) {
+  for (list.front(); list.currPos() < list.length(); list.next()) {
+    console.log(list.getElement());
+  }
 }
 
 //Customer Objects
+function displayList(list) {
+  for (list.front(); list.currPos() < list.length(); list.next()) {
+    if (list.getElement() instanceof Customer) {
+      console.log(
+        list.getElement()["name"] + ", " + list.getElement()["movie"]
+      );
+    } else {
+      console.log(list.getElement());
+    }
+  }
+}
 
+//Create a list to store the customers who check out movies at the kiosk
+function Customer(name, movie) {
+  this.name = name;
+  this.movie = movie;
+}
 
+//Function to checkout a movie
+function checkOut(name, movie, filmList, customerList) {
+  if (movieList.contains(movie)) {
+    var c = new Customer(name, movie);
+    customerList.append(c);
+    filmList.remove(movie);
+  } else {
+    console.log(movie + " is not available.");
+  }
+}
